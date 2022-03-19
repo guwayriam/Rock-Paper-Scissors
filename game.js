cfe83f7de504daf3c3cc12ab996708a12b15c032
Guwayria <guwayriam@gmail.com>
2022-03-18 19:13:42 -0700

    work on the logic for the game

--- game.js
+++ game.js
@@ -1,10 +1,25 @@
 function playRound(playerSelection, computerSelection){
     // logic to compare the winner of the 
     //rock paper scissor game
+    let round = null;
+    console.log(round);
+    if (playerSelection === 'ROCK' && computerSelection === 'SCISSOR'
+            || playerSelection === 'PAPER' && computerSelection === 'ROCK'
+            || playerSelection === 'SCISSOR' && computerSelection === 'ROCK'){
+
+        round ++;
+
+    }else if (playerSelection === 'SCISSOR' && computerSelection === 'ROCK'
+            || playerSelection === 'ROCK' && computerSelection === 'PAPER'
+            || playerSelection === 'PAPER' && computerSelection === 'SCISSOR'){
+
+        round --;
+    }else{
+        round = null;
+    }
+    console.log(round);
+    return round;
     
-
-    console.log(playerSelection.toUpperCase(), computerSelection);
-
 }
 
 function computerPlay(randomNumber){
@@ -24,15 +39,44 @@ function computerPlay(randomNumber){
     return computerSelection;
 }
 
-let choice = prompt('Choose ROCK, PAPER, or SCISSOR');
 
-let num = Math.random() *3;
-let random = Math.floor(num);
+let round = 0;
+let game = 0
 
-let play = computerPlay(random);
 
-playRound(choice, play);
 
-console.log(num);
-console.log(random);
-console.log(play);
+while( Math.abs(round) < 3  && game < 5){
+
+    let choice = prompt('Choose ROCK, PAPER, or SCISSOR');
+    let num = Math.random() *3;
+    let random = Math.floor(num);
+
+    if(choice != null){
+        let play = computerPlay(random);
+        let win = playRound(choice.toUpperCase(), play);
+        if(win > 0){
+            console.log('Player won')
+        }else if(win < 0){
+            console.log('Computer won')
+        }else (
+            console.log('Tie Tie')
+        )
+        round + win
+        game ++;
+
+    }else{
+        console.log('Wrong entery. Try again');
+    }
+
+    
+}
+
+//
+
+
+
+
+
+//console.log(num);
+//console.log(random);
+//console.log(play);
