ca48c2db3cb64f59b24311a01360215d70313ef8
Guwayria <guwayriam@gmail.com>
2022-04-18 14:00:38 -0700

    added a few more functions

--- game.js
+++ game.js
@@ -1,2 +1,29 @@
-const userScore = document.getElementById("userscore");
-console.log('hello')
+let userScore = 0;
+let compScore = 0;
+const userScore_Span = document.getElementById("userscore");
+const compScore_Span = document.getElementById("computerScore");
+const scoreBoard_Div = document.querySelector(".scoreboard");
+const result = document.querySelector(".result");
+const rock_Div = document.getElementById("r");
+const paper_Div = document.getElementById("p");
+const scissor_Div = document.getElementById("s");
+const message = document.getElementById("message");
+
+function game(userChoice){
+    let compChoice = compChoice();
+}
+
+function compChoice(){
+    let choice =['r','p','s'];
+    let rand = Math.floor(Math.random()* 3)
+    console.log(choice[rand]);
+    console.log(rand)
+    return choice[rand];
+}
+
+
+rock_Div.addEventListener("click", function(){
+   // game("r")
+    //console.log(compChoice);
+    compChoice()
+})
